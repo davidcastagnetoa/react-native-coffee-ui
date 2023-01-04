ac9e7df2c30c101cf5b5a874bbf065ca12b25c77
David Castagneto A <davidcastagnetoa@gmail.com>
2023-01-04 06:27:33 +0100

    coffee UI app

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import colors from "./app/config/colors"
+import HomeScreen from "./app/screens/HomeScreen";
+import CoffeeDetailsScreen from "./app/screens/CoffeeDetailsScreen";
+import coffees from "./app/config/coffees";
+
+const App = () => {
+  return (
+    <View style={{flex:1, backgroundColor: colors.dark}}>
+      {/* <CoffeeDetailsScreen coffee={coffees[2]}/> */}
+      <HomeScreen/>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
